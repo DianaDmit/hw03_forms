173ab0915a3b60dc6b4165f9a8d0f54791b7318b
Диана Дмитриева <dahsar@yandex.ru>
2022-04-19 20:52:06 +0300

    Forms-66

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,19 @@
-from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
 
-from .models import Post, Group, User
+from .models import Post, Group
+from .forms import PostForm, CommentForm
+
+User = get_user_model()
+
+PAGE = 10
 
 
 def index(request):
-    posts = Post.objects.all().order_by('-pub_date')
+    posts = Post.objects.all()
     paginator = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -21,7 +29,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     # условия WHERE group_id = {group_id}
-    posts = Post.objects.all()
+    posts = group.posts.all()
     paginator = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -35,32 +43,63 @@ def group_posts(request, slug):
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
-    posts = Post.objects.filter(author=author)
-    paginator = Paginator(posts, 10)
+    post_list = Post.objects.filter(author=author).order_by('-pub_date')
+    post_count = post_list.count()
+    paginator = Paginator(post_list, PAGE)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    postcount = posts.count
     context = {
-        'posts': posts,
-        'username': username,
-        'pag_number': request.GET.get('page'),
+        'author': author,
+        'post_count': post_count,
+        'post_list': post_list,
         'page_obj': page_obj,
-        'postcount': postcount,
-        'author': author
-
     }
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    author = post.author
-    author_posts = author.posts
-    count_posts = author_posts.count()
+    posts = Post.objects.select_related('author', 'group').get(pk=post_id)
+    posts_numbers = Post.objects.filter(author=all.user).count()
+    author_posts = posts.author.posts.count()
     context = {
-        'author': author,
-        'title': post.text,
-        'post': post,
-        'count_posts': count_posts,
+        'posts': posts,
+        'posts_numbers': posts_numbers,
+        'author_posts': author_posts,
     }
     return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            form.save()
+            return redirect('posts:profile', request.user)
+        return render(request, 'posts/create_post.html', {'form': form})
+    form = PostForm()
+    return render(request, 'posts/create_post.html', {'form': form})
+
+
+@login_required()
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    is_edit = True
+    template = 'posts/create_post.html'
+    if request.user == post.author:
+        form = PostForm(
+            request.POST or None,
+            files=request.FILES or None,
+            instance=post
+        )
+        if form.is_valid():
+            form.save()
+            return redirect('posts:post_detail', post_id=post_id)
+    context = {
+        'post_id': post_id,
+        'form': form,
+        'is_edit': is_edit
+    }
+    return render(request, template, context)
