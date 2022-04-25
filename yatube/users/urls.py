c2df4d9318cd7336b06114e1f2fbeddde81534b4
Диана Дмитриева <dahsar@yandex.ru>
2022-04-25 17:41:49 +0300

    Date 25042022-1

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,21 +1,32 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView
 from django.urls import path
-
 from . import views
-
 
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    # Форма регистрации на сайте
+    path(
+        'signup/', views.SignUp.as_view(template_name='users/signup.html'),
+        name='signup'
+    ),
+    # Форма после выхода из учетной записи
     path(
         'logout/',
         LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
     ),
+    # Форма входа на сайт
     path(
         'login/',
         LoginView.as_view(template_name='users/login.html'),
         name='login'
+    ),
+    # Форма сброса пароля
+    path(
+        'passwordreset/',
+        PasswordResetView.as_view(template_name='users/'
+                                                'password_reset_form.html'),
+        name='password_reset_form'
     ),
 ]
