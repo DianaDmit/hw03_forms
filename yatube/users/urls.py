4bb319a0e8c1c38d91b8d692c48a51a5a4bfb640
Диана Дмитриева <dahsar@yandex.ru>
2022-04-29 18:00:19 +0300

    'Date 29/04/2022-2'

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -13,19 +13,19 @@ urlpatterns = [
     path('signup/',
          views.SignUp.as_view(template_name='users/signup.html'),
          name='signup'
-    ),
+        ),
     # Форма после выхода из учетной записи
     path(
         'logout/',
         LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
-    ),
+        ),
     # Форма входа на сайт
     path(
         'login/',
         LoginView.as_view(template_name='users/login.html'),
         name='login'
-    ),
+        ),
     # Форма сброса пароля
     path('password_reset/',
          PasswordResetView.as_view
@@ -40,18 +40,17 @@ urlpatterns = [
 
     path('password_change/done/', PasswordChangeDoneView.as_view
     (template_name='users/password_change_done.html'),
-
-    ),
+        ),
 
     path('password_reset/done/', PasswordResetDoneView.as_view
     (template_name='users/password_reset_done.html'),
-    ),
+        ),
 
     path('reset/<uidb64>/<token>/', PasswordChangeView.as_view
     (template_name='users/password_reset_confirm.html'),
-    ),
+        ),
 
     path('reset/done/', PasswordResetCompleteView.as_view
     (template_name='users/password_reset_complete.html'),
-    ),
+        ),
 ]
